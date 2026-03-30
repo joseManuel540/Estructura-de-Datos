e7c989779e7325bac9f44b6bbd24a9ff1ba4acb2
joseManuel540 <josenoob312@gmail.com>
2026-03-30 17:36:25 -0500

    cambios

--- corte 2/ListasSimplesEnlazadas/ListasEnlazadasSimples/src/Vagon.java
+++ corte 2/ListasSimplesEnlazadas/ListasEnlazadasSimples/src/Vagon.java
@@ -1,18 +1,1 @@
-/*La Clase Vagon (Nodo): Debe contener contenido (String), pesoToneladas (double), origen (String) y destino (String).
- */
 
-public class Vagon {
-    String contenido;
-    double pesoToneladas;
-    String origen;
-    String destino;
-    Vagon siguiente;
-
-    public Vagon(String contenido, double pesoToneladas, String origen, String destino){
-        this.contenido = contenido;
-        this.pesoToneladas = pesoToneladas;
-        this.origen = origen;
-        this.destino = destino;
-        this.siguiente = null;
-    }
-}
